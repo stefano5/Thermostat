1f6c86658e48829b5e0b31b67b949dc47daae6db
stefano maugeri <stefano@debian.stefano.legion>
2020-12-31 16:33:33 +0100

    update 1

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ sudo chown $USER:$USER /var/www/html
 sudo chmod u+w /var/www/html
 
 echo "Step 3) load website"
-sudo rm -r /var/www/html/
+sudo rm -r /var/www/html/*
 sudo cp Website/* /var/www/html/
 
 echo "Step 4) init directory"
 $PATHTHERMOSTAT=$HOME/.thermostat
 
 mkdir $PATHTHERMOSTAT
-rm -rf /$PATHTHERMOSTAT
+rm -rf /$PATHTHERMOSTAT >> /dev/zero
 cp Thermostat_JAVA/dist/*.jar $PATHTHERMOSTAT/Thermostat.jar
 
 echo "Step 5) compile runner"
@@ -27,7 +27,8 @@ cp thermostat $PATHTHERMOSTAT/
 sudo ln -s $PATHTHERMOSTAT/thermostat /usr/bin/
 
 echo "Step 6) create service with systemctl"
-sudo cp  thermostat.service /etc/systemd/system/
+sudo rm /etc/systemd/system >> /dev/zero
+sudo cp thermostat.service /etc/systemd/system/
 sudo systemctl enable thermostat.service
 sudo systemctl start thermostat.service
 sudo systemctl status thermostat.service
