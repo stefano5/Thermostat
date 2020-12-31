9288442232f2863b77497b12437538fcee90c806
stefano maugeri <stefano@debian.stefano.legion>
2020-12-31 16:27:31 +0100

    load new files

--- install.sh
+++ install.sh
@@ -5,12 +5,30 @@ sudo apt upgrade
 
 echo "Step 2) install apache2. Press RETURN to continue"
 read
-sudo apt install apache2 
+sudo apt install apache2 gcc
 sudo chown $USER:$USER /var/www/html
 sudo chmod u+w /var/www/html
 
 echo "Step 3) load website"
+sudo rm -r /var/www/html/
 sudo cp Website/* /var/www/html/
 
-cp Thermostat_JAVA/dist/*.jar Thermostat
+echo "Step 4) init directory"
+$PATHTHERMOSTAT=$HOME/.thermostat
+
+mkdir $PATHTHERMOSTAT
+rm -rf /$PATHTHERMOSTAT
+cp Thermostat_JAVA/dist/*.jar $PATHTHERMOSTAT/Thermostat.jar
+
+echo "Step 5) compile runner"
+gcc -Wall -Werror thermostat.c -o thermostat
+
+cp thermostat $PATHTHERMOSTAT/
+sudo ln -s $PATHTHERMOSTAT/thermostat /usr/bin/
+
+echo "Step 6) create service with systemctl"
+sudo cp  thermostat.service /etc/systemd/system/
+sudo systemctl enable thermostat.service
+sudo systemctl start thermostat.service
+sudo systemctl status thermostat.service
 
---
+++ thermostat.c
@@ -0,0 +1,64 @@
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <dirent.h>
+#include    <pwd.h>
+#include    <unistd.h>
+#include    <errno.h>
+#include    <stdint.h>
+#include    <sys/types.h>
+#include    <sys/stat.h>
+#include    <unistd.h>
+
+#define COMMAND "cd /home/%s/.thermostat && java -jar Thermostat.jar"
+
+
+
+/////////////////////////   COPIED BY OTHER PERSONAL LIBRARY
+#define TRUE    1
+
+void initArray_str(char *array, int dim) {
+    for (int i=0; i<dim; i++) {
+        array[i] = '\0';
+    }
+}
+
+
+/* 
+ * If 'disableRoot == TRUE' if you run with sudo this function return uid 1000 by default
+ * you can use NO_ROOT_GUI  
+ * */
+char user[32];
+int getUserId(char *user, int disableRoot) {
+    int uid = getuid();
+    initArray_str(user, 32);
+    if (uid == 0 && disableRoot == TRUE) {       //trattiamo l'user come semplice utente
+        uid = 1000;
+        //printf_d("[apiSO.c]->getUserId sei l'utente root ma sei stato disattivatto e sei stato trattato come user 1000\n");
+        printf("[apiSO.c]->getUserId user root is disabled, now you are user 1000\n");
+    }
+
+    if (getpwuid(uid)->pw_name == NULL) {
+        printf("getpwuid failed, what is your user id? [userID@namePC:~$]\n>");
+        //readString(user, 32); useless
+    } else {
+        strcpy(user, getpwuid(uid)->pw_name);
+    }
+    return uid;
+}
+
+/////////////////////////
+
+
+int main(int argc, char **argv) {
+    char *command;
+    getUserId(user, TRUE);
+    command = (char*)malloc(strlen(COMMAND) + strlen(user) + 1);
+    initArray_str(command, strlen(command)); 
+    sprintf(command, COMMAND, user);
+
+    //printf("[%s]\n", command);
+	system(command);
+    free(command);
+	exit(EXIT_SUCCESS);
+}
